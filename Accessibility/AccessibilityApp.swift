047964a9eaa33503c1eb3c868b960155cad4e248
Esben Viskum <viskum@viskum.dk>
2021-05-16 15:31:42 +0200

    Initial Commit

---
+++ Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Esben Viskum on 16/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
